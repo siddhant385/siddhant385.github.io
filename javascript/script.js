bd34e77cb3f643f95f5bedf1179854b16f2bab62
siddhant385 <ssiddhant.ssharma@gmail.com>
2024-09-11 01:18:38 +0530

    V2.0

---
+++ javascript/script.js
@@ -0,0 +1,26 @@
+
+var typed = new Typed('#passionate', {
+    strings: ['<strong>passionate</strong>^5000', '<strong>Enthusiastic</strong>^5000','<strong>Zealous</strong>^5000','<strong>Devoted</strong>^5000'],
+    typeSpeed: 100,
+    loop:true,
+    smartBackspace: true,
+    backDelay: 700,
+    backSpeed: 100,
+    // fadeOut: true,
+    // fadeOutClass: 'typed-fade-out',
+    // fadeOutDelay: 500,
+
+    
+  
+});
+
+// var typed = new Typed('#heading', {
+//     strings: ['<strong>WORLD</strong>^5000', '<strong>SITE</strong>^5000'],
+//     typeSpeed: 200,
+//     loop:true,
+//     smartBackspace: true,
+//     showCursor: true,
+//     cursorChar: '_',
+//     autoInsertCss: true,
+//     backSpeed:100,
+// });
