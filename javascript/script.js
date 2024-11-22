1fb5f67c39bde3af8db3e5ea79c0f3abf02dea25
siddhant385 <ssiddhant.ssharma@gmail.com>
2024-11-22 14:06:12 +0530

    Updated Responsiveness

--- javascript/script.js
+++ javascript/script.js
@@ -14,6 +14,19 @@ var typed = new Typed('#passionate', {
   
 });
 
+const prev = document.getElementById('prev-btn')
+const next = document.getElementById('next-btn')
+const list = document.getElementById('item-list')
+const itemWidth = 150
+const padding = 10
+
+prev.addEventListener('click',()=>{
+  list.scrollLeft -= (itemWidth + padding)
+})
+next.addEventListener('click',()=>{
+  list.scrollLeft += (itemWidth + padding)
+})
+
 // var typed = new Typed('#heading', {
 //     strings: ['<strong>WORLD</strong>^5000', '<strong>SITE</strong>^5000'],
 //     typeSpeed: 200,
