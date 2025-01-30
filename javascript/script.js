bd0a350767117f7ae238abf5f059151197fd7756
siddhant385 <ssiddhant.ssharma@gmail.com>
2025-01-31 04:46:59 +0530

    Created Skills Tab and modified it

--- javascript/script.js
+++ javascript/script.js
@@ -37,5 +37,37 @@ next.addEventListener('click',()=>{
 //     autoInsertCss: true,
 //     backSpeed:100,
 // });
+// var pythonpercent = Number(getElementById('python').innerHTML)
+// var csspercent = Number(getElementById('css').innerHTML)
+// var dockerpercent = Number(getElementById('docker').innerHTML)
+// var htmlpercent = Number(getElementById('html').innerHTML)
+// var javascriptpercent = Number(getElementById('js').innerHTML)
 
 
+
+// var csspercent = getElementById('')
+function Progress(id,percent){
+    var progress = new ProgressBar.Line('.'+id, {
+      strokeWidth: 4,
+      easing: 'easeInOut',
+      duration: 1400,
+      color: '#ffc800',
+      trailColor: '#eee',
+      trailWidth: 1,
+      svgStyle: {width: '100%', height: '100%'},
+      from: {color: '#00000'},
+      to: {color: '#ffc800'},
+    });
+    console.log(percent)
+    progress.animate(percent)
+  
+}
+  Progress('python-percent',document.getElementById('python-percent').innerHTML.split('%')[0]/100)
+  Progress('css-percent',document.getElementById('css-percent').innerHTML.split('%')[0]/100)
+  Progress('docker-percent',document.getElementById('docker-percent').innerHTML.split('%')[0]/100)
+  Progress('html-percent',document.getElementById('html-percent').innerHTML.split('%')[0]/100)
+  Progress('javascript-percent',document.getElementById('javascript-percent').innerHTML.split('%')[0]/100)
+  Progress('bash-percent',document.getElementById('bash-percent').innerHTML.split('%')[0]/100)
+  
+  
+  
